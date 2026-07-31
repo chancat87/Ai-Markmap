4da992bf9b6c9baa6ed5d6522b37c124990226f4
chancattk <25543219+chancat87@users.noreply.github.com>
2026-07-31 08:49:54 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# 部署时把外部 CDN 链接替换为本地文件（只影响部署产物，不动 git 仓库）
-sed -i 's#https://unpkg.com/d3@7.8.5/dist/d3.min.js#/libs/d3.min.js#' index.html
-sed -i 's#https://unpkg.com/markmap-lib@0.15.3/dist/browser/index.js#/libs/markmap-lib.js#' index.html
-sed -i 's#https://unpkg.com/markmap-view@0.15.3/dist/browser/index.js#/libs/markmap-view.js#' index.html
+# 生成部署产物到 public/ 目录（不影响 git 仓库里的文件）
+mkdir -p public
+cp index.html public/
+cp -r libs public/
+sed -i 's#https://unpkg.com/d3@7.8.5/dist/d3.min.js#/libs/d3.min.js#' public/index.html
+sed -i 's#https://unpkg.com/markmap-lib@0.15.3/dist/browser/index.js#/libs/markmap-lib.js#' public/index.html
+sed -i 's#https://unpkg.com/markmap-view@0.15.3/dist/browser/index.js#/libs/markmap-view.js#' public/index.html
